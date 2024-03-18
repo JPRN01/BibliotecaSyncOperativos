94e9a1fcade074c22cb137bafe08baeb185e8619
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-03-17 22:53:06 -0600

    Update sync.c
    Parte Angello

--- sync.c
+++ sync.c
@@ -1,4 +1,5 @@
 #include "sync.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 void init_semaphore(Semaphore *sem, int initialCount) {
@@ -45,4 +46,50 @@ void destroy_semaphore(Semaphore *sem) {
         sem->waiting_list = wt->next;
         pthread_cond_destroy(&wt->cond);
     }
+}
+
+// Implementación de funciones de monitor
+void monitor_init(Monitor *monitor) {
+    pthread_mutex_init(&monitor->mutex, NULL);
+    pthread_cond_init(&monitor->cond, NULL);
+    monitor->busy = 0;
+}
+
+void monitor_enter(Monitor *monitor) {
+    pthread_mutex_lock(&monitor->mutex);
+    while (monitor->busy) {
+        printf("Thread %ld is waiting.\n", pthread_self());
+        pthread_cond_wait(&monitor->cond, &monitor->mutex);
+    }
+    monitor->busy = 1;
+    pthread_mutex_unlock(&monitor->mutex);
+}
+
+void monitor_exit(Monitor *monitor) {
+    pthread_mutex_lock(&monitor->mutex);
+    monitor->busy = 0;
+    pthread_cond_signal(&monitor->cond);
+    pthread_mutex_unlock(&monitor->mutex);
+}
+
+// Implementación de funciones de barrera
+void barrier_init(Barrier *barrier, int count) {
+    barrier->count = count;
+    barrier->arrived = 0;
+    pthread_mutex_init(&barrier->mutex, NULL);
+    pthread_cond_init(&barrier->cond, NULL);
+}
+
+void barrier_sync(Barrier *barrier) {
+    pthread_mutex_lock(&barrier->mutex);
+    barrier->arrived++;
+
+    if (barrier->arrived == barrier->count) {
+        barrier->arrived = 0; 
+        pthread_cond_broadcast(&barrier->cond); // Despierta a todos los hilos en espera
+    } else {
+        // Espera hasta que el último hilo llegue a la barrera
+        pthread_cond_wait(&barrier->cond, &barrier->mutex);
+    }
+    pthread_mutex_unlock(&barrier->mutex);
 }
