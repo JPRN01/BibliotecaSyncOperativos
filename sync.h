57c4e44b8b9acd09b2ee789fb6d20db01ff02fc7
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-03-17 22:54:15 -0600

    Update sync.h
    Parte Angello

--- sync.h
+++ sync.h
@@ -14,9 +14,33 @@ typedef struct {
     WaitingThread *waiting_list;
 } Semaphore;
 
+// Estructura para un monitor
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    int busy;
+} Monitor;
+
+// Estructura para una barrera
+typedef struct {
+    int count;
+    int arrived;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} Barrier;
+
 void init_semaphore(Semaphore *sem, int initialCount);
 void wait_semaphore(Semaphore *sem);
 void post_semaphore(Semaphore *sem);
 void destroy_semaphore(Semaphore *sem);
+
+// Funciones para operaciones de monitor
+void monitor_init(Monitor *monitor);
+void monitor_enter(Monitor *monitor);
+void monitor_exit(Monitor *monitor);
+
+// Funciones para operaciones de barrera
+void barrier_init(Barrier *barrier, int count);
+void barrier_sync(Barrier *barrier);
 
 #endif
